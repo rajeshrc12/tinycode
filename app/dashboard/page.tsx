85327753cc4e131f4490161bfa6ba2120751e63f
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-04-30 11:27:06 +0530

    reactflow added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,13 +2,16 @@ import Sidebar from "@/components/sidebar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Workflow from "@/components/workflow";
+import Link from "next/link";
 import React from "react";
 
 const DashboardPage = () => {
   return (
     <div className="flex flex-col gap-5 py-5 px-15">
       <div className="flex justify-end">
-        <Button>Create Workflow</Button>
+        <Link href={"dashboard/workflow"}>
+          <Button>Create Workflow</Button>
+        </Link>
       </div>
       <div className="flex justify-end">
         <Input placeholder="search workflows" className="bg-white w-[300px]" />
---
+++ app/dashboard/workflow/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useCallback } from "react";
+import {
+  addEdge,
+  Background,
+  Connection,
+  Controls,
+  Edge,
+  Node,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+} from "@xyflow/react";
+
+import "@xyflow/react/dist/style.css";
+const initialNodes: Node[] = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  { id: "3", position: { x: 0, y: 400 }, data: { label: "3" } },
+];
+const initialEdges: Edge[] = [
+  { id: "e1-2", source: "1", target: "2", animated: true },
+];
+const WorkflowPage = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback((connection: Connection) => {
+    const edge: Edge = {
+      ...connection,
+      animated: true,
+      id: `e${edges.length + 1}`,
+      sourceHandle: connection.sourceHandle ?? null,
+      targetHandle: connection.targetHandle ?? null,
+    };
+    setEdges((prevEdges) => addEdge(edge, prevEdges));
+  }, []);
+  return (
+    <div className="w-full h-screen">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+};
+export default WorkflowPage;
