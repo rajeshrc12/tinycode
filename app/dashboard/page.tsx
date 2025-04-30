861629af57579d8e5042147ae5229c83992f9385
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-04-30 10:53:11 +0530

    dashboard page added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,21 @@
 import Sidebar from "@/components/sidebar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Workflow from "@/components/workflow";
 import React from "react";
 
 const DashboardPage = () => {
   return (
-    <div>
-      <div>Dashboard</div>
+    <div className="flex flex-col gap-5 py-5 px-15">
+      <div className="flex justify-end">
+        <Button>Create Workflow</Button>
+      </div>
+      <div className="flex justify-end">
+        <Input placeholder="search workflows" className="bg-white w-[300px]" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Workflow />
+      </div>
     </div>
   );
 };
---
+++ components/workflow.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Workflow = () => {
+  return (
+    <div className="bg-white border px-2 py-4 rounded-xl flex flex-col">
+      <span className="text-sm">Workflow 1</span>
+      <span className="text-xs text-muted-foreground">created at today</span>
+    </div>
+  );
+};
+
+export default Workflow;
